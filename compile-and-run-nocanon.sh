178a7d349a5fcc992747ac26da0d598053b000cf
Angelo Terzi <angeloterzi98@gmail.com>
2026-07-13 18:35:12 +0200

    Added no canonicalization build scripts for reference

---
+++ compile-and-run-nocanon.sh
@@ -0,0 +1,92 @@
+#! /bin/sh
+
+set -e
+
+if [ $# -ne 3 ];
+then 
+	echo "ERROR: wrong amount of parameters, the call syntax for this script is: ./compile-and-run.sh main.c kernel.mlir compiled.elf" 1>&2
+	exit 1
+fi
+
+MAINC=$1
+KERNELMLIR=$2
+COMPILED=$3
+
+if ! [ -f "$MAINC" ];
+then
+	echo "ERROR: \"$MAINC\" is not a file!" 1>&2
+	exit 2
+fi
+
+if ! test -f "$KERNELMLIR";
+then
+	echo "ERROR: \"$KERNELMLIR\" is not a file!" 1>&2
+	exit 3
+fi
+
+MAINEXT=${MAINC##*.}
+
+if test "$MAINEXT" != "c" -a "$MAINEXT" != "cc" -a "$MAINEXT" != "cpp" -a "$MAINEXT" != "C" -a "$MAINEXT" != "CC" -a "$MAINEXT" != "CPP";
+then
+	echo "ERROR: \"$MAINC\" is not a C/C++ file! (check by extension)" 1>&2
+	exit 4
+fi
+
+KERNELEXT=${KERNELMLIR##*.}
+
+if test "$KERNELEXT" != "mlir" -a "$KERNELEXT" != "MLIR";
+then
+	echo "ERROR: \"$KERNELMLIR\" is not an mlir file! (check by extension)" 1>&2
+	exit 5
+fi
+
+MAINS=${MAINC%.c*}.S
+MAINO=${MAINS%.S}.o
+KERNELS=${KERNELMLIR%.mlir}.S
+KERNELO=${KERNELS%.S}.o
+
+
+# Compile C to snitch native riscv
+/opt/snitch-llvm/bin/clang++ -Wno-unused-command-line-argument -menable-experimental-extensions -mcpu=snitch -mabi=ilp32d -mcmodel=medany \
+						     -ftls-model=local-exec -ffast-math -fno-builtin-printf -fno-common -O3 -D__DEFINED_uint64_t \
+						     -I/opt/snitch_cluster/target/snitch_cluster/sw/runtime/rtl/src -I/opt/snitch_cluster/target/snitch_cluster/sw/runtime/common \
+						     -I/opt/snitch_cluster/sw/snRuntime/api -I/opt/snitch_cluster/sw/snRuntime/src -I/opt/snitch_cluster/sw/snRuntime/src/omp/ \
+						     -I/opt/snitch_cluster/sw/snRuntime/api/omp/ -I/opt/snitch_cluster/sw/math/arch/riscv64/bits/ -I/opt/snitch_cluster/sw/math/arch/generic \
+						     -I/opt/snitch_cluster/sw/math/src/include -I/opt/snitch_cluster/sw/math/src/internal -I/opt/snitch_cluster/sw/math/include/bits \
+						     -I/opt/snitch_cluster/sw/math/include \
+						     -S -x c++ -o $MAINS $MAINC
+# Compile mlir to snitch native riscv
+/usr/bin/mlir-opt-16 -opaque-pointers=0 -linalg-generalize-named-ops -eliminate-empty-tensors -empty-tensor-to-alloc-tensor \
+					     -one-shot-bufferize='bufferize-function-boundaries function-boundary-type-conversion=identity-layout-map allow-return-allocs' \
+					     -canonicalize $KERNELMLIR \
+			| sed "s/arith.maxf/arith.maximumf/g" \
+			| xdsl-opt -p arith-add-fastmath \
+			| sed "s/arith.maxf/arith.maximumf/g" \
+			| xdsl-opt -p canonicalize,convert-linalg-to-memref-stream,memref-stream-legalize,canonicalize,memref-stream-infer-fill,memref-stream-unnest-out-parameters,memref-stream-fold-fill,memref-stream-generalize-fill,memref-stream-interleave,memref-stream-tile-outer-loops{target_rank=4},memref-streamify,convert-memref-stream-to-loops,canonicalize,scf-for-loop-flatten,canonicalize,convert-memref-to-riscv-snitch,convert-memref-to-riscv,lower-affine,convert-scf-to-riscv-scf,convert-arith-to-riscv-snitch,convert-arith-to-riscv,convert-func-to-riscv-func,convert-memref-stream-to-snitch-stream,reconcile-unrealized-casts,convert-riscv-scf-for-to-frep,snitch-allocate-registers,convert-snitch-stream-to-snitch,lower-snitch,riscv-scf-loop-range-folding,riscv-allocate-registers,convert-riscv-scf-to-riscv-cf -t riscv-asm -o $KERNELS
+# Compile main to object
+/opt/snitch-llvm/bin/clang++ -menable-experimental-extensions -mcpu=snitch -mabi=ilp32d -mcmodel=medany -ftls-model=local-exec \
+					      -ffast-math -fno-builtin-printf -fno-common -O3 -D__DEFINED_uint64_t \
+					      -I/opt/snitch_cluster/target/snitch_cluster/sw/runtime/rtl/src -I/opt/snitch_cluster/target/snitch_cluster/sw/runtime/common \
+					      -I/opt/snitch_cluster/sw/snRuntime/api -I/opt/snitch_cluster/sw/snRuntime/src -I/opt/snitch_cluster/sw/snRuntime/src/omp/ \
+					      -I/opt/snitch_cluster/sw/snRuntime/api/omp/ -I/opt/snitch_cluster/sw/math/arch/riscv64/bits/ \
+					      -I/opt/snitch_cluster/sw/math/arch/generic -I/opt/snitch_cluster/sw/math/src/include -I/opt/snitch_cluster/sw/math/src/internal \
+					      -I/opt/snitch_cluster/sw/math/include/bits -I/opt/snitch_cluster/sw/math/include \
+					      -c -o $MAINO $MAINS
+# Compile mlir to object
+/opt/snitch-llvm/bin/clang++ -menable-experimental-extensions -mcpu=snitch -mabi=ilp32d -mcmodel=medany -ftls-model=local-exec \
+					      -ffast-math -fno-builtin-printf -fno-common -O3 -D__DEFINED_uint64_t \
+					      -I/opt/snitch_cluster/target/snitch_cluster/sw/runtime/rtl/src -I/opt/snitch_cluster/target/snitch_cluster/sw/runtime/common \
+					      -I/opt/snitch_cluster/sw/snRuntime/api -I/opt/snitch_cluster/sw/snRuntime/src -I/opt/snitch_cluster/sw/snRuntime/src/omp/ \
+					      -I/opt/snitch_cluster/sw/snRuntime/api/omp/ -I/opt/snitch_cluster/sw/math/arch/riscv64/bits/ \
+					      -I/opt/snitch_cluster/sw/math/arch/generic -I/opt/snitch_cluster/sw/math/src/include -I/opt/snitch_cluster/sw/math/src/internal \
+					      -I/opt/snitch_cluster/sw/math/include/bits -I/opt/snitch_cluster/sw/math/include \
+					      -c -o $KERNELO $KERNELS
+# Link everything
+/opt/snitch-llvm/bin/clang++ -fuse-ld=/opt/snitch-llvm/bin/ld.lld -nostartfiles -nostdlib -L/opt/snitch-llvm/lib/clang/15.0.0/lib/ \
+				     -L/opt/snitch_cluster/target/snitch_cluster/sw/runtime/ -L/opt/snitch_cluster/target/snitch_cluster/sw/runtime/rtl \
+				     -L/opt/snitch_cluster/target/snitch_cluster/sw/runtime/rtl/build -T/opt/snitch_cluster/sw/snRuntime/base.ld \
+				     -lc -lsnRuntime -lclang_rt.builtins-riscv32 \
+				     -o $COMPILED $MAINO $KERNELO
+
+# Run code on simulator
+/opt/snitch-rtl/bin/snitch_cluster.vlt $COMPILED
---
+++ start-and-run-nocanon.sh
@@ -0,0 +1,115 @@
+#! /bin/sh
+
+# Check that we are given the right amount of parameters
+if [ $# -ne 4 -a $# -ne 5 ];
+then 
+	echo "ERROR: wrong amount of parameters, the call syntax for this script is:\n{...}/start-and-run.sh {...}/riscv-docker main.c [data.h] kernel.mlir main.elf" 1>&2
+	exit 1
+fi
+
+docker --version > /dev/null 2>&1
+
+#Check that docker exists and can be run
+if [ $? -ne 0 ];
+then
+	echo "ERROR: Docker does not seem to be installed, please install it before running this script!"
+	exit 2
+fi
+
+set -e
+
+MAINC=$2
+DATAH=""
+KERNELMLIR=""
+COMPILED=""
+
+if [ $# -eq 5 ];
+then
+	DATAH=$3
+	KERNELMLIR=$4
+	COMPILED=$5
+else
+	KERNELMLIR=$3
+	COMPILED=$4
+fi
+
+# Check that parameters correspond to the correct stuff
+
+if test $# -eq 5 && ! test -f $DATAH;
+then
+	echo "ERROR: \"$DATAH\" is not a regular file!" 1>&2
+	exit 2
+fi
+
+if ! [ -f $MAINC ];
+then
+	echo "ERROR: \"$MAINC\" is not a regular file!" 1>&2
+	exit 3
+fi
+
+if ! test -f $KERNELMLIR;
+then
+	echo "ERROR: \"$KERNELMLIR\" is not a regular file!" 1>&2
+	exit 4
+fi
+
+DATAEXT=${DATAH##*.}
+
+if test $# -eq 5 && test $DATAEXT != "h" -a $DATAEXT != "H";
+then
+	echo "ERROR: \"$DATAH\" is not a C/C++ header file! (check by extension)" 1>&2
+	exit 5
+fi
+
+MAINEXT=${MAINC##*.}
+
+if test $MAINEXT != "c" -a $MAINEXT != "cc" -a $MAINEXT != "cpp" -a $MAINEXT != "C" -a $MAINEXT != "CC" -a $MAINEXT != "CPP";
+then
+	echo "ERROR: \"$MAINC\" is not a C/C++ file! (check by extension)" 1>&2
+	exit 6
+fi
+
+KERNELEXT=${KERNELMLIR##*.}
+
+if test $KERNELEXT != "mlir" -a $KERNELEXT != "MLIR";
+then
+	echo "ERROR: \"$KERNELMLIR\" is not an mlir file! (check by extension)" 1>&2
+	exit 7
+fi
+
+REPO=$1
+
+if ! [ -d $REPO ];
+then
+	echo "ERROR: $REPO should be a directory, but it's not!" 1>&2
+	exit 8
+fi
+
+LAST_DIR=$(pwd)
+cd $REPO
+REPO=$(pwd)
+cd $LAST_DIR
+
+VENV="$REPO/.venv-docker"
+
+# Delete old venv if present
+if [ -d $VENV ];
+then 
+	echo "Removing old Venv"
+	sudo rm -rf $VENV
+fi
+
+# Copy files into the riscv-paper-experiments repo
+cp ./compile-and-run-nocanon.sh $REPO
+
+cp $2 $REPO
+
+cp $3 $REPO
+
+if [ $# -eq 5 ];
+then
+	cp $4 $REPO
+fi
+
+# Start the vm and run the compilation script
+docker run --rm -ti --volume $REPO:/src ghcr.io/opencompl/snitch-toolchain:4.0.0 /bin/bash -c "cd /src; ./compile-and-run-nocanon.sh $MAINC $KERNELMLIR $COMPILED"
